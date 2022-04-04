da0c98827943c527defac9e2830785e7dd79526a
Juanjo78git <juanjo78@gmail.com>
2022-04-04 10:10:14 +0200

    PoC

--- pyCTelebot/pyPoC.py
+++ pyCTelebot/pyPoC.py
@@ -6,12 +6,25 @@ import random
 import sys
 from telegram import Update
 from telegram.ext import CallbackContext
+from pyCTelebot.config.auth import ENV_CONFIG
 
 # i18n
 _ = gettext.gettext
 
 # Logs
 logger = logging.getLogger(__name__)
+# DEBUG / INFO / WARNING / ERROR / CRITICAL
+if ENV_CONFIG.get('log') == 'CRITICAL':
+    logger.setLevel(logging.CRITICAL)
+elif ENV_CONFIG.get('log') == 'ERROR':
+    logger.setLevel(logging.ERROR)
+elif ENV_CONFIG.get('log') == 'WARNING':
+    logger.setLevel(logging.WARNING)
+elif ENV_CONFIG.get('log') == 'INFO':
+    logger.setLevel(logging.INFO)
+else:
+    logger.setLevel(logging.DEBUG)
+# Force DEBUG
 logger.setLevel(logging.DEBUG)
 
 
@@ -25,6 +38,15 @@ def run_telegram(update: Update, context: CallbackContext):
                                                                                       update.message.text,
                                                                                       len(context.args)),
                level=logging.DEBUG)
+    if logger.level == logging.DEBUG:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='/poc User {0} ({1}) - chat: {2} ({3}) - message ({4}) {5}'.format(
+                                     update.effective_user.name,
+                                     update.effective_user.id,
+                                     update.effective_chat.title,
+                                     update.effective_chat.id,
+                                     update.message.text,
+                                     len(context.args)))
     # Do something
 
     logger.log(msg='pyPoC run_telegram stop ID: {0}'.format(seed), level=logging.INFO)
