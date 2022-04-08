e7a5ec024d1577ec66522e095ebea51867f8a773
Juanjo78git <juanjo78@gmail.com>
2022-04-08 08:26:36 +0200

    prices

--- initial_ddl.sql
+++ initial_ddl.sql
@@ -9,29 +9,47 @@ CREATE TABLE IF NOT EXISTS users (
 	secret VARCHAR,
 	passphrase VARCHAR
 );
+
+
 CREATE TABLE IF NOT EXISTS strategies (
 	strategy_id VARCHAR(50) NOT NULL,
-	buy_in_callback NUMERIC,
+	description VARCHAR,
   PRIMARY KEY (strategy_id)
+);
+CREATE TABLE IF NOT EXISTS strategy_symbols (
+	strategy_id VARCHAR(50) NOT NULL,
+	exchange VARCHAR(50) NOT NULL,
+	symbol VARCHAR(50) NOT NULL,
+	unit_value NUMERIC,
+	take_profit NUMERIC,
+	buy_in_callback NUMERIC,
+    PRIMARY KEY (strategy_id, exchange, symbol),
+    FOREIGN KEY (strategy_id)
+      REFERENCES strategies (strategy_id)
 );
 CREATE TABLE IF NOT EXISTS strategy_steps (
 	strategy_id VARCHAR(50) NOT NULL,
 	step INTEGER NOT NULL,
-	margin NUMERIC NOT NULL,
-	units NUMERIC NOT NULL,
+	step_type VARCHAR(50) NOT NULL,
+	margin NUMERIC,
+	units NUMERIC,
   PRIMARY KEY (strategy_id, step),
   FOREIGN KEY (strategy_id)
       REFERENCES strategies (strategy_id)
 );
+
+
 CREATE TABLE IF NOT EXISTS active_strategies (
 	user_id VARCHAR(50) NOT NULL,
 	strategy_id VARCHAR(50) NOT NULL,
 	symbol VARCHAR(50) NOT NULL,
 	unit_value NUMERIC NOT NULL,
-	profit NUMERIC,
+	take_profit NUMERIC,
 	status VARCHAR(50) NOT NULL,
-	real_invest NUMERIC,
-	real_profit NUMERIC,
+	current_invest NUMERIC,
+	current_profit NUMERIC,
+	start_audit_date TIMESTAMP,
+	end_audit_date TIMESTAMP,
 	PRIMARY KEY (user_id, strategy_id),
 	FOREIGN KEY (user_id)
       REFERENCES users (user_id),
@@ -43,13 +61,15 @@ CREATE TABLE IF NOT EXISTS active_strategy_steps (
 	strategy_id VARCHAR(50) NOT NULL,
 	symbol VARCHAR(50) NOT NULL,
 	step INTEGER NOT NULL,
+	step_type VARCHAR(50) NOT NULL,
 	unit_value NUMERIC NOT NULL,
-	profit NUMERIC,
-	margin NUMERIC NOT NULL,
-	units NUMERIC NOT NULL,
+	take_profit NUMERIC,
+	margin NUMERIC,
+	units NUMERIC,
 	status VARCHAR(50) NOT NULL,
 	real_price NUMERIC,
 	order_id VARCHAR,
+	order_audit_date TIMESTAMP,
 	PRIMARY KEY (user_id, strategy_id, step),
 	FOREIGN KEY (user_id, strategy_id)
       REFERENCES active_strategies (user_id, strategy_id)
@@ -63,6 +83,8 @@ CREATE TABLE IF NOT EXISTS exchange_prices (
 	last_sell_price NUMERIC,
 	current_sell_price NUMERIC,
 	sell_price_variation_percentage NUMERIC,
+	last_audit_date TIMESTAMP,
+	current_audit_date TIMESTAMP,
 	PRIMARY KEY (exchange, symbol)
 );
 commit;
